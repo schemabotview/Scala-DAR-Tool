04cc4309d54b898ad4a21af3c77788082c6a068e
maddipotiganesh <maddipoti.ganesh@gmail.com>
2025-11-17 12:09:52 +0000

    Remove unused filter and map methods from Dataset class

--- src/main/scala/in/schemabot/dar/module3oops/models/Dataset.scala
+++ src/main/scala/in/schemabot/dar/module3oops/models/Dataset.scala
@@ -1,10 +1,4 @@
 package in.schemabot.dar.module3oops.models
 
 case class Dataset(rows: List[Row]):
-  def filter(p: Row => Boolean): Dataset =
-    Dataset(rows.filter(p))
-
-  def map(f: Row => Row): Dataset =
-    Dataset(rows.map(f))
-
   def size: Int = rows.size
